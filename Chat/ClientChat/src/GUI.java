aee86212be7f9bc5e9b0c4d8241f798111baaf89
Adolf <justus.fab@gmail.com>
2014-10-15 12:19:06 +0200

    added prepacket

--- Chat/ClientChat/src/GUI.java
+++ Chat/ClientChat/src/GUI.java
@@ -18,74 +18,74 @@ import java.awt.event.ActionEvent;
 
 
 public class GUI extends JFrame {
-	private JTextField textField;
-	private JButton btnSend;
-	private boolean canSend = false;
-	/**
-	 * @wbp.nonvisual location=-290,194
-	 */
-	
+  private JTextField textField;
+  private JButton btnSend;
+  private boolean canSend = false;
+  /**
+   * @wbp.nonvisual location=-290,194
+   */
+  
 
-	/**
-	 * Launch the application.
-	 */
+  /**
+   * Launch the application.
+   */
 
-	/**
-	 * Create the frame.
-	 */
-	public GUI() {
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 782, 493);
-		getContentPane().add(panel);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 782, Short.MAX_VALUE)
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
-					.addContainerGap())
-		);
-		panel.setLayout(gl_panel);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 494, 782, 59);
-		getContentPane().add(panel_1);
-		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
-		
-		textField = new JTextField();
-		panel_1.add(textField);
-		textField.setColumns(10);
-		
-		btnSend = new JButton("Send");
-		btnSend.setAlignmentY(Component.TOP_ALIGNMENT);
-		panel_1.add(btnSend);
-		pack();
-	}
-	
-	public String getText(){
-		canSend = false;
-		return textField.getText().toString();
-	}
+  /**
+   * Create the frame.
+   */
+  public GUI() {
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 800, 600);
+    getContentPane().setLayout(null);
+    
+    JPanel panel = new JPanel();
+    panel.setBounds(0, 0, 782, 493);
+    getContentPane().add(panel);
+    
+    JLabel lblNewLabel = new JLabel("New label");
+    GroupLayout gl_panel = new GroupLayout(panel);
+    gl_panel.setHorizontalGroup(
+      gl_panel.createParallelGroup(Alignment.LEADING)
+        .addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 782, Short.MAX_VALUE)
+    );
+    gl_panel.setVerticalGroup(
+      gl_panel.createParallelGroup(Alignment.LEADING)
+        .addGroup(gl_panel.createSequentialGroup()
+          .addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
+          .addContainerGap())
+    );
+    panel.setLayout(gl_panel);
+    
+    JPanel panel_1 = new JPanel();
+    panel_1.setBounds(0, 494, 782, 59);
+    getContentPane().add(panel_1);
+    panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+    
+    textField = new JTextField();
+    panel_1.add(textField);
+    textField.setColumns(10);
+    
+    btnSend = new JButton("Send");
+    btnSend.setAlignmentY(Component.TOP_ALIGNMENT);
+    panel_1.add(btnSend);
+    //pack();
+  }
+  
+  public String getText(){
+    canSend = false;
+    return textField.getText().toString();
+  }
 
-	public boolean canSend() {
-		return canSend;
-	}
-	
-	public void setSendButtonListener(ActionListener listener)
-	{
-		if (btnSend.getActionListeners().length > 0)
-			btnSend.removeActionListener(btnSend.getActionListeners()[0]);
-		
-		btnSend.addActionListener(listener);
-	}
+  public boolean canSend() {
+    return canSend;
+  }
+  
+  public void setSendButtonListener(ActionListener listener)
+  {
+    if (btnSend.getActionListeners().length > 0)
+      btnSend.removeActionListener(btnSend.getActionListeners()[0]);
+    
+    btnSend.addActionListener(listener);
+  }
 }
--- Chat/ClientChat/src/Start.java
+++ Chat/ClientChat/src/Start.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 
-
-
 public class Start {
-	
-	
-	public static void main(String[] args) {
-		System.out.print("Enter the IP you want to connect to:");
-		//"192.168.178.22"
-		new ChatSocket(new Scanner(System.in).next(), 1026);
-	}
+  
+  
+  public static void main(String[] args) {
+    System.out.print("Enter the IP you want to connect to:");
+    //"192.168.178.22"
+    //(new Scanner(System.in).next()
+    new ChatSocket("10.32.111.6", 80);
+  }
 }
