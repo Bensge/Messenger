6310a5503aac6c081f72dfa7abca4805411a66b8
Benno <bensge@me.com>
2014-10-18 23:22:38 +0200

    MASSIVE input read worker improvements

--- Chat/ClientChat/src/ServerReadingWorker.java
+++ Chat/ClientChat/src/ServerReadingWorker.java
@@ -1,6 +1,7 @@
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.SwingWorker;
@@ -19,29 +20,66 @@ public class ServerReadingWorker extends SwingWorker<Void, ChatPacket> {
 	}
 	
 	@Override
-	protected Void doInBackground() throws Exception {
-		// TODO Auto-generated method stub
+	protected Void doInBackground() throws Exception
+	{
 		byte[] prePacket = new byte[MessengerCommon.INT_FIELD_SIZE * 2];
 		
-	    System.out.println("Listening to server messages!");
+	    System.out.println("Listening to client messages!");
 	    System.out.println("Now ready!");
 	    while (true)
 	    {
-	        in.read(prePacket,0,8);
-	        System.out.println("Received Pre-Packet!" + prePacket);
+	    	//Clean out prePacket for the case that it's filled with the old package.
+	    	Arrays.fill( prePacket, (byte) 0 );
+	        int result = in.read(prePacket,0,8);
+	        
+	        if (result == -1)
+	        {
+	        	System.out.println("Client disconnected!");
+	        	//It's been great talking to you, Client!
+	        	break;
+	        }
 	        
 	        int packetType = MessengerCommon.intFromBuffer(prePacket, 0);
+	        
+	        if (packetType == 0)
+	        {
+	        	//Got invalid packet
+	        	continue;
+	        }
+	        
+	        System.out.println("Received Pre-Packet!");
 	        System.out.println("Packet type: " + packetType);
 	        
 	        int packetSize = MessengerCommon.intFromBuffer(prePacket, 4);
 	        System.out.println("Packet size: " + packetSize);
 	        
 	        byte[] packetBuffer = new byte[packetSize];
-	        in.read(packetBuffer,0,packetSize);
 	        
+	        result = 0;
+	        int receivedCount = 0;
+	        while (receivedCount < packetSize)
+	        {
+		        result = in.read(packetBuffer,receivedCount,packetSize - receivedCount);
+		        if (result < 0)
+		        {
+		        	System.out.println("Error reading big packet!!!" + result);
+		        	break;
+		        }
+		        else
+		        {
+		        	receivedCount += result;
+		        	if (receivedCount < packetSize)
+			        {
+			        	System.out.println("Reading progress: " + ((float)receivedCount / (float)packetSize * 100.f) + "%");
+			        }
+		        }
+	        }
 	        ChatPacket packet = ChatPacket.parseDataPacket(prePacket, packetBuffer);
+	        
 	        publish(packet);
 	    }
+	    
+	    return null;
 	}
 	
 	@Override
